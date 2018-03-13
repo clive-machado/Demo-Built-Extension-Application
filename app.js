513bc66c0da806f9120d99d09098b4d4e8910583
Clive Machado <clive.machado@raweng.com>
2018-03-13 17:31:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('blta14c94311365670a')
+var app = Built.App('bltb62a403faff09784')
 .setHost("dev-api.built.io")
 .setProtocol("https")
-.setMasterKey('blt77ed42a5de276223')
+.setMasterKey('blta0f3474316380510')
 
 var extensionSDK = app.Extension({
 	secret_key     : 'cliveblt',
-	extension_key	 : 'blt_ext_default_zip',
+	extension_key	 : 'blt_ext_default',
 	static         : __dirname + '/client', 
 	routes         : require('./server/routes')
 }) 
